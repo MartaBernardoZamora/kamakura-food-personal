18c67ae3242395e5e3bc72b4ae070cb2454a859b
MartaBernardoZamora <martabernardozamora@gmail.com>
2024-12-28 21:31:15 +0100

    feature/sidebarToggle complete

--- src/events.js
+++ src/events.js
@@ -2,12 +2,13 @@
 import{products, filters} from "./data/data.js";
 import {clearHtml, printFilters, printMenu} from "./components/menu.js";
 import {changeFilter} from "./components/searcher.js";
-import {productAddToCart} from "./components/cart.js";
+import {displayCart, productAddToCart} from "./components/cart.js";
 
 document.addEventListener("DOMContentLoaded", function() {
     clearHtml();
     printFilters(filters);
     printMenu(products);
+    document.getElementById('cart').addEventListener('click', displayCart);
 });
 const setFilterButtonEvent = (button) =>{
     button.addEventListener('click', changeFilter);
