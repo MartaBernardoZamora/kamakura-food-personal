0e771b7fa9da5715dde6b48ea32ea5a58e01c8d1
MartaBernardoZamora <martabernardozamora@gmail.com>
2024-12-28 21:14:36 +0100

    feature/filtersPrint complete

--- src/components/searcher.js
+++ src/components/searcher.js
@@ -1,3 +1,7 @@
 //DEBE buscar los productos por los filtros
+const changeFilter = () => {
+    alert("aqui irá un evento");
+}
 
+export{changeFilter}
 
--- src/events.js
+++ src/events.js
@@ -1,13 +1,19 @@
 //Intenta separar los eventos en este archivo.
 import{products, filters} from "./data/data.js";
-import {clearHtml, printMenu} from "./components/menu.js";
+import {clearHtml, printFilters, printMenu} from "./components/menu.js";
+import {changeFilter} from "./components/searcher.js";
 import {productAddToCart} from "./components/cart.js";
 
 document.addEventListener("DOMContentLoaded", function() {
     clearHtml();
+    printFilters(filters);
     printMenu(products);
 });
+const setFilterButtonEvent = (button) =>{
+    button.addEventListener('click', changeFilter);
+}
 const setAddButtonEvent = (button) =>{
     button.addEventListener('click', productAddToCart);
 }
-export {setAddButtonEvent}
+
+export {setFilterButtonEvent, setAddButtonEvent}
